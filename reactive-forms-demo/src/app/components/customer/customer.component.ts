0bdc0c74896dad46f7bcfd8f8fb997b409881e6d
duchiha <duchiha@example.com>
2019-12-28 04:10:04 +0100

    building basic reactive form

--- reactive-forms-demo/src/app/components/customer/customer.component.ts
+++ reactive-forms-demo/src/app/components/customer/customer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
+signUpForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signUpForm = new FormGroup({
+           firstName:new FormControl(),
+         lastName: new FormControl(),
+         email: new FormControl(),
+         sendCatalog: new FormControl(true),
+    })
+  }
+
+  save():void{
+    console.log('Saving...');
   }
 
 }
